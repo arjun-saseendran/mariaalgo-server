ecc125d71a154d0fb133b45d90ce4afe86171481
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-03-05 18:28:58 +0530

    fix data

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /root/mariaalgo-server
-echo "🚀 Starting Maria Algo server at 8:30 AM..."
-pm2 start server.js --name maria-algo
--- stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "🛑 Stopping Maria Algo server at 3:35 PM..."
-pm2 stop maria-algo
